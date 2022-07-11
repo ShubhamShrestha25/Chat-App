5cae91c9727e6a2469950f0698991526c55243a8
ShubhamShrestha25 <shubhamshrestha25@gmail.com>
2022-07-11 10:50:54 +0545

    Register Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-
+import { Route, Routes } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/utils/APIRoutes.js
@@ -0,0 +1,2 @@
+const host = "http://localhost:8001";
+export const registerRoute = `${host}/api/auth/register`;
